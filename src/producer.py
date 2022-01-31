906514dcff92e3411793a67ad117b0e708a3f224
Axel Neergaard <axel.git@neergaard.dev>
2022-01-31 04:23:04 +0100

    Lint cleanup producer

--- src/producer.py
+++ src/producer.py
@@ -21,13 +21,12 @@ def _value_serializer(dict_value):
 def _format_domain_to_url(domain):
     if 'http' in domain or 'https' in domain:
         return domain
-    else:
-        return f'http://{domain}'
+    return f'http://{domain}'
 
 
 def _ping_domain(domain, timeout=10):
     url = _format_domain_to_url(domain)
-    response = requests.get(url, timeout=10)
+    response = requests.get(url, timeout=timeout)
     return {
         'url': url,
         'status_code': response.status_code,
@@ -36,9 +35,9 @@ def _ping_domain(domain, timeout=10):
 
 def _on_future_done(future):
     if future.done():
-        _logger.info(f'Future job done successfully')
+        _logger.info('Future job done successfully')
     else:
-        _logger.info(f'Future job cancelled')
+        _logger.info('Future job cancelled')
 
 
 def _produce(thread_pool_executor, kafka_producer):
@@ -55,7 +54,7 @@ def _produce(thread_pool_executor, kafka_producer):
         except requests.Timeout:
             _logger.warning('Reaching domain timed out', extra={'domain': domain})
         except Exception as exc:
-            _logger.exception(f'Reaching domain failed', {'domain': domain}, exc_info=exc)
+            _logger.exception('Reaching domain failed', extra={'domain': domain}, exc_info=exc)
         else:
             kafka_producer.send(_config.topic, value=data)
             future.add_done_callback(_on_future_done)
